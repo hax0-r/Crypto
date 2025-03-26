f35db6f0cf9446fa819388032868598fd1514586
Muhammad Talha <talha18513@gmail.com>
2025-03-26 10:07:47 +0500

    languages

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,6 +8,7 @@ import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
     const [navbar, setNavbar] = useState(false)
+    const [isOpenLanguageDropDown, setIsOpenLanguageDropDown] = useState(false)
 
     const { t, i18n } = useTranslation();
 
@@ -15,6 +16,16 @@ const Navbar = () => {
 
     const changeLanguage = (lang: string) => {
         i18n.changeLanguage(lang);
+    }
+
+    const english = () => {
+        changeLanguage('en')
+        setIsOpenLanguageDropDown(false)
+    }
+
+    const urdu = () => {
+        changeLanguage('ur')
+        setIsOpenLanguageDropDown(false)
     }
 
     useEffect(() => {
@@ -39,17 +50,21 @@ const Navbar = () => {
                         <li><NavLink to={"/faq"}>Faqs</NavLink></li>
                     </ul> */}
 
-                    <div className="flex items-center z-10 md:gap-10 gap-4 group">
+                    <div className="flex items-center z-10 md:gap-10 gap-4">
                         <div className="relative">
-                            <div className="flex font-medium md:text-lg cursor-pointer items-center gap-2">
+                            <div onClick={() => setIsOpenLanguageDropDown(!isOpenLanguageDropDown)} className="flex font-medium md:text-lg cursor-pointer items-center gap-2">
                                 <p>{storedLanguage}</p>
                                 <IoIosArrowDown />
                             </div>
 
-                            <div className="absolute w-20 top-8 hidden -left-4 group-hover:block fadeIn p-2 rounded-lg bg-[#5f29b7]">
-                                <button onClick={() => changeLanguage('en')} className="hover:bg-[#7641cd] w-full transition-all duration-500 cursor-pointer px-3 py-1.5 rounded-lg">en</button>
-                                <button onClick={() => changeLanguage('ur')} className="hover:bg-[#7641cd] w-full transition-all duration-500 cursor-pointer px-3 py-1.5 rounded-lg">ur</button>
-                            </div>
+                            {
+                                isOpenLanguageDropDown && (
+                                    <div className="absolute w-20 top-8 -left-4 fadeIn p-2 rounded-lg bg-[#5f29b7]">
+                                        <button onClick={english} className="hover:bg-[#7641cd] w-full transition-all duration-500 cursor-pointer px-3 py-1.5 rounded-lg">en</button>
+                                        <button onClick={urdu} className="hover:bg-[#7641cd] w-full transition-all duration-500 cursor-pointer px-3 py-1.5 rounded-lg">ur</button>
+                                    </div>
+                                )
+                            }
 
                         </div>
                         <Link to={"/sign-in"} className="bg-[#5f29b7] flex items-center gap-3 md:px-7 px-5 cursor-pointer transition-all duration-500 hover:bg-[#5f29b7]/80 md:py-3 py-2 rounded-full">{t("signIn")}<FaArrowRightLong className="lg:flex hidden" />
@@ -57,11 +72,12 @@ const Navbar = () => {
                         {/* <FaBars onClick={() => setNavbar(true)} className="text-2xl cursor-pointer lg:hidden" /> */}
                     </div>
                 </div>
-            </div>
+            </div >
 
             {/* responsive navbar */}
 
-            <div className={`fixed bg-black text-white overflow-hidden transition-all duration-500 top-0 ${navbar ? "w-full" : "w-0"} left-0 z-50`}>
+            < div className={`fixed bg-black text-white overflow-hidden transition-all duration-500 top-0 ${navbar ? "w-full" : "w-0"} left-0 z-50`
+            }>
                 <div className=" w-full px-10 relative pt-6 min-h-screen">
                     <IoClose onClick={() => setNavbar(false)} className='absolute cursor-pointer top-6 right-7 text-3xl' />
                     <img src={logo} alt="logo" className='h-10' />
@@ -73,7 +89,7 @@ const Navbar = () => {
                         <li><NavLink onClick={() => setNavbar(false)} className="text-nowrap" to={"/contact-us"}>Contact us</NavLink></li>
                     </ul>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
