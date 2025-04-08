77460120423923868cb2582615f5600b55997302
Muhammad Talha <talha18513@gmail.com>
2025-04-08 14:50:02 +0500

    complete

--- src/Pages/Dashboard/AdminDashboard/EventsAdd.tsx
+++ src/Pages/Dashboard/AdminDashboard/EventsAdd.tsx
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router";
 import { Button } from "../../../Components/ui/button"
 import { Input } from "../../../Components/ui/input"
 import { GoGift } from "react-icons/go";
+import { BiArrowBack } from "react-icons/bi";
+
 const EventsAdd = () => {
+    const navigate = useNavigate()
     return (
         <>
             <div className="bg-[#171022] w-full min-h-screen p-5">
-
+                <button onClick={() => navigate(-1)} className='flex items-center gap-3 text-white mb-3 opacity-80 cursor-pointer'><BiArrowBack /> Back               </button>
                 <h3 className="text-3xl text-[#ffffffe5] mb-1">
                     Add Event
                 </h3>
